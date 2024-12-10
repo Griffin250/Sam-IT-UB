c4d7c1ed75ab6faaf94264e7299cf67869da4d29
Griffin250 <afs0024458@agderskolen.no>
2024-12-10 11:41:40 +0100

    New Updates

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,12 +1,54 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
+import ServiceImage from '../../src/assets/otherImages/SupportCommunity.png'
+
 const Services = () => {
   const { t} = useTranslation();
 
 
   return (
     <div className="bg-gradient-to-r from-blue-200 via-gray-100 to-blue-200 py-12 px-6 md:px-16 lg:px-24 mt-12">
+
+    <section className="min-h-screen flex items-center justify-center py-12">
+      <div className="container mx-auto px-6 md:px-12 lg:px-24 flex flex-col md:flex-row items-center">
+        
+        {/* Left Column (Text) */}
+        <div className="md:w-2/3 text-center md:text-left mb-8 md:mb-0">
+          <h2 className="text-4xl md:text-5xl font-extrabold leading-tight mb-4 text-gray-600">
+            The Service You Deserve
+            <span className="block text-6xl md:text-6xl mt-4 font-bold text-gray-800">
+              The Service We Give!
+            </span>
+          </h2>
+          <p className="text-xl md:text-2xl mb-8">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <div className="flex justify-center md:justify-start gap-4">
+            <button className="px-6 py-3 bg-blue-500 rounded-lg text-white text-lg font-medium hover:bg-blue-700 transition-colors">
+              Find Out
+            </button>
+            <button className="px-6 py-3 bg-transparent border-2 border-orange-600 rounded-lg text-blue-700 text-lg font-medium hover:bg-orange-600 hover:text-white transition-all">
+              Live Demo
+            </button>
+          </div>
+        </div>
+
+        {/* Right Column (Image) */}
+        <div className="md:w-1/2 flex justify-center">
+          <img
+            src={ServiceImage} // Replace with the actual image URL
+            alt="Hero"
+            className="w-full h-auto rounded-lg shadow-md "
+          />
+        </div>
+      </div>
+    </section>
+
+
+    
+
       <div className="max-w-4xl mx-auto">
 
         {/* Heading */}
